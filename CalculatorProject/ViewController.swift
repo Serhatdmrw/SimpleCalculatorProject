945662038ed58bd62e21528fbdc28f46eb1f627f
Serhat Demir <Serhatdmrw@gmail.com>
2023-03-27 21:14:57 +0300

    [feat] Create simple calculator project.

--- CalculatorProject/ViewController.swift
+++ CalculatorProject/ViewController.swift
@@ -9,11 +9,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outles
+    @IBOutlet private weak var firstText: UITextField!
+    @IBOutlet private weak var seconderText: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - Actions
+    @IBAction func sumClicked(_ sender: Any) {
+        sum()
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        minus()
+    }
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        multiply()
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        divide()
+    }
 }
 
+// MARK: - Helpers
+private extension ViewController {
+    
+    func sum() {
+        guard let myFirstNumber = firstText.text , let mySeconderNumber = seconderText.text else { return }
+        if let firstNumber = Int(myFirstNumber) {
+            if let seconderNumber = Int(mySeconderNumber) {
+             let result = firstNumber + seconderNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    func minus() {
+        guard let myFirstNumber = firstText.text , let mySeconderNumber = seconderText.text else { return }
+        if let firstNumber = Int(myFirstNumber) {
+            if let seconderNumber = Int(mySeconderNumber) {
+             let result = firstNumber - seconderNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    func multiply() {
+        guard let myFirstNumber = firstText.text , let mySeconderNumber = seconderText.text else { return }
+        if let firstNumber = Int(myFirstNumber) {
+            if let seconderNumber = Int(mySeconderNumber) {
+             let result = firstNumber * seconderNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    func divide() {
+        guard let myFirstNumber = firstText.text , let mySeconderNumber = seconderText.text else { return }
+        if let firstNumber = Int(myFirstNumber) {
+            if let seconderNumber = Int(mySeconderNumber) {
+             let result = firstNumber / seconderNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+}
